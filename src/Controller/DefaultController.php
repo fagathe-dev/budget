f04923734304f6b60014dd3617303dea64c73e62
fagathe-dev <122600633+fagathe-dev@users.noreply.github.com>
2023-03-29 15:36:30 +0200

    feat(templates): template de base (#2)
    Co-authored-by: AGATHE Frédérick <fagathe@meilleurtaux.com>

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,19 @@
+<?php 
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class DefaultController extends AbstractController
+{
+
+    public function __construct(){}
+
+    #[Route('/', name: 'app_default')] 
+    public function default():Response 
+    {
+
+        return $this->render('default/index.html.twig');
+    }
+}
