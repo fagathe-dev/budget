c95ede1dec8c862c6dfde36096216ca7524650f8
AGATHE Frédérick <fagathe@meilleurtaux.com>
2023-03-31 04:14:50 +0200

    feat(admin-user)

---
+++ src/Controller/Admin/CategoryController.php
@@ -0,0 +1,22 @@
+<?php 
+namespace App\Controller\Admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/admin/category', name: 'admin_category_')]
+class CategoryController extends AbstractController
+{
+    
+    public function __construct(
+        // private UserService $service
+    ){}
+
+    #[Route('', name: 'index', methods: ['GET'])]
+    public function index():Response 
+    {
+        return $this->render('admin/category/index.html.twig');
+    }
+
+}
